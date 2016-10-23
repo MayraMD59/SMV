2e6f12306cea5e2cd9f804c4c9d4c61c30e16c8c
MaryNest <MaryNest@MARYNEST-PC>
2016-10-23 13:36:42 -0500

    Se agrega interfaz en espera de confirmacion de datos faltantes

--- pruebas/PruebasVentanas/ListaGrupos.Designer.cs
+++ pruebas/PruebasVentanas/ListaGrupos.Designer.cs
@@ -28,11 +28,342 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+			System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			this.Alumnos = new FlatUI.FlatTabControl();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.formSkin1 = new FlatUI.FormSkin();
+			this.flatMini1 = new FlatUI.FlatMini();
+			this.flatMax1 = new FlatUI.FlatMax();
+			this.flatClose1 = new FlatUI.FlatClose();
+			this.tabPage3 = new System.Windows.Forms.TabPage();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.flatLabel1 = new FlatUI.FlatLabel();
+			this.flatLabel2 = new FlatUI.FlatLabel();
+			this.flatLabel3 = new FlatUI.FlatLabel();
+			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.iTalk_LinkLabel1 = new iTalk.iTalk_LinkLabel();
+			this.iTalk_RichTextBox1 = new iTalk.iTalk_RichTextBox();
+			this.flatLabel4 = new FlatUI.FlatLabel();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.flatTabControl1 = new FlatUI.FlatTabControl();
+			this.tabPage4 = new System.Windows.Forms.TabPage();
+			this.tabPage5 = new System.Windows.Forms.TabPage();
+			this.Alumnos.SuspendLayout();
+			this.tabPage2.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.formSkin1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			this.flatTabControl1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// Alumnos
+			// 
+			this.Alumnos.ActiveColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(168)))), ((int)(((byte)(109)))));
+			this.Alumnos.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(49)))));
+			this.Alumnos.Controls.Add(this.tabPage1);
+			this.Alumnos.Controls.Add(this.tabPage2);
+			this.Alumnos.Controls.Add(this.tabPage3);
+			this.Alumnos.Font = new System.Drawing.Font("Segoe UI", 10F);
+			this.Alumnos.ItemSize = new System.Drawing.Size(120, 40);
+			this.Alumnos.Location = new System.Drawing.Point(0, 50);
+			this.Alumnos.Name = "Alumnos";
+			this.Alumnos.SelectedIndex = 0;
+			this.Alumnos.Size = new System.Drawing.Size(746, 421);
+			this.Alumnos.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+			this.Alumnos.TabIndex = 0;
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(70)))), ((int)(((byte)(73)))));
+			this.tabPage2.Controls.Add(this.flatTabControl1);
+			this.tabPage2.Location = new System.Drawing.Point(4, 44);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(738, 373);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "ListaGrupos";
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(70)))), ((int)(((byte)(73)))));
+			this.tabPage1.Controls.Add(this.pictureBox2);
+			this.tabPage1.Controls.Add(this.flatLabel4);
+			this.tabPage1.Controls.Add(this.iTalk_RichTextBox1);
+			this.tabPage1.Controls.Add(this.iTalk_LinkLabel1);
+			this.tabPage1.Controls.Add(this.chart1);
+			this.tabPage1.Controls.Add(this.flatLabel3);
+			this.tabPage1.Controls.Add(this.flatLabel2);
+			this.tabPage1.Controls.Add(this.flatLabel1);
+			this.tabPage1.Controls.Add(this.pictureBox1);
+			this.tabPage1.Location = new System.Drawing.Point(4, 44);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(738, 373);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "Alumnos";
+			// 
+			// formSkin1
+			// 
+			this.formSkin1.BackColor = System.Drawing.Color.White;
+			this.formSkin1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(70)))), ((int)(((byte)(73)))));
+			this.formSkin1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(58)))), ((int)(((byte)(60)))));
+			this.formSkin1.Controls.Add(this.flatClose1);
+			this.formSkin1.Controls.Add(this.flatMax1);
+			this.formSkin1.Controls.Add(this.flatMini1);
+			this.formSkin1.Controls.Add(this.Alumnos);
+			this.formSkin1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.formSkin1.FlatColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(168)))), ((int)(((byte)(109)))));
+			this.formSkin1.Font = new System.Drawing.Font("Segoe UI", 12F);
+			this.formSkin1.HeaderColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(49)))));
+			this.formSkin1.HeaderMaximize = false;
+			this.formSkin1.Location = new System.Drawing.Point(0, 0);
+			this.formSkin1.Name = "formSkin1";
+			this.formSkin1.Size = new System.Drawing.Size(746, 471);
+			this.formSkin1.TabIndex = 0;
+			this.formSkin1.Text = "App";
+			// 
+			// flatMini1
+			// 
+			this.flatMini1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.flatMini1.BackColor = System.Drawing.Color.White;
+			this.flatMini1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(49)))));
+			this.flatMini1.Font = new System.Drawing.Font("Marlett", 12F);
+			this.flatMini1.Location = new System.Drawing.Point(618, 13);
+			this.flatMini1.Name = "flatMini1";
+			this.flatMini1.Size = new System.Drawing.Size(18, 18);
+			this.flatMini1.TabIndex = 1;
+			this.flatMini1.Text = "flatMini1";
+			this.flatMini1.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+			// 
+			// flatMax1
+			// 
+			this.flatMax1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.flatMax1.BackColor = System.Drawing.Color.White;
+			this.flatMax1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(49)))));
+			this.flatMax1.Font = new System.Drawing.Font("Marlett", 12F);
+			this.flatMax1.Location = new System.Drawing.Point(660, 12);
+			this.flatMax1.Name = "flatMax1";
+			this.flatMax1.Size = new System.Drawing.Size(18, 18);
+			this.flatMax1.TabIndex = 2;
+			this.flatMax1.Text = "flatMax1";
+			this.flatMax1.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+			// 
+			// flatClose1
+			// 
+			this.flatClose1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.flatClose1.BackColor = System.Drawing.Color.White;
+			this.flatClose1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+			this.flatClose1.Font = new System.Drawing.Font("Marlett", 10F);
+			this.flatClose1.Location = new System.Drawing.Point(705, 13);
+			this.flatClose1.Name = "flatClose1";
+			this.flatClose1.Size = new System.Drawing.Size(18, 18);
+			this.flatClose1.TabIndex = 3;
+			this.flatClose1.Text = "flatClose1";
+			this.flatClose1.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+			// 
+			// tabPage3
+			// 
+			this.tabPage3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(70)))), ((int)(((byte)(73)))));
+			this.tabPage3.Location = new System.Drawing.Point(4, 44);
+			this.tabPage3.Name = "tabPage3";
+			this.tabPage3.Size = new System.Drawing.Size(738, 373);
+			this.tabPage3.TabIndex = 2;
+			this.tabPage3.Text = "VistaGeneral";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(3, 33);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(79, 78);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// flatLabel1
+			// 
+			this.flatLabel1.AutoSize = true;
+			this.flatLabel1.BackColor = System.Drawing.Color.Transparent;
+			this.flatLabel1.Font = new System.Drawing.Font("Segoe UI", 8F);
+			this.flatLabel1.ForeColor = System.Drawing.Color.White;
+			this.flatLabel1.Location = new System.Drawing.Point(101, 33);
+			this.flatLabel1.Name = "flatLabel1";
+			this.flatLabel1.Size = new System.Drawing.Size(53, 13);
+			this.flatLabel1.TabIndex = 1;
+			this.flatLabel1.Text = "Alumno1";
+			// 
+			// flatLabel2
+			// 
+			this.flatLabel2.AutoSize = true;
+			this.flatLabel2.BackColor = System.Drawing.Color.Transparent;
+			this.flatLabel2.Font = new System.Drawing.Font("Segoe UI", 8F);
+			this.flatLabel2.ForeColor = System.Drawing.Color.White;
+			this.flatLabel2.Location = new System.Drawing.Point(6, 3);
+			this.flatLabel2.Name = "flatLabel2";
+			this.flatLabel2.Size = new System.Drawing.Size(45, 13);
+			this.flatLabel2.TabIndex = 2;
+			this.flatLabel2.Text = "Genero";
+			// 
+			// flatLabel3
+			// 
+			this.flatLabel3.AutoSize = true;
+			this.flatLabel3.BackColor = System.Drawing.Color.Transparent;
+			this.flatLabel3.Font = new System.Drawing.Font("Segoe UI", 8F);
+			this.flatLabel3.ForeColor = System.Drawing.Color.White;
+			this.flatLabel3.Location = new System.Drawing.Point(101, 74);
+			this.flatLabel3.Name = "flatLabel3";
+			this.flatLabel3.Size = new System.Drawing.Size(111, 13);
+			this.flatLabel3.TabIndex = 3;
+			this.flatLabel3.Text = "Enrolado en Grupos";
+			// 
+			// chart1
+			// 
+			chartArea4.Name = "ChartArea1";
+			this.chart1.ChartAreas.Add(chartArea4);
+			legend4.Name = "Legend1";
+			this.chart1.Legends.Add(legend4);
+			this.chart1.Location = new System.Drawing.Point(104, 126);
+			this.chart1.Name = "chart1";
+			series4.ChartArea = "ChartArea1";
+			series4.Legend = "Legend1";
+			series4.Name = "Series1";
+			this.chart1.Series.Add(series4);
+			this.chart1.Size = new System.Drawing.Size(436, 104);
+			this.chart1.TabIndex = 4;
+			this.chart1.Text = "chart1";
+			// 
+			// iTalk_LinkLabel1
+			// 
+			this.iTalk_LinkLabel1.ActiveLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(101)))), ((int)(((byte)(202)))));
+			this.iTalk_LinkLabel1.AutoSize = true;
+			this.iTalk_LinkLabel1.BackColor = System.Drawing.Color.Transparent;
+			this.iTalk_LinkLabel1.Font = new System.Drawing.Font("Segoe UI", 8F);
+			this.iTalk_LinkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+			this.iTalk_LinkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(225)))));
+			this.iTalk_LinkLabel1.Location = new System.Drawing.Point(114, 242);
+			this.iTalk_LinkLabel1.Name = "iTalk_LinkLabel1";
+			this.iTalk_LinkLabel1.Size = new System.Drawing.Size(91, 13);
+			this.iTalk_LinkLabel1.TabIndex = 5;
+			this.iTalk_LinkLabel1.TabStop = true;
+			this.iTalk_LinkLabel1.Text = "Grupos Inscritos";
+			this.iTalk_LinkLabel1.VisitedLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(101)))), ((int)(((byte)(202)))));
+			// 
+			// iTalk_RichTextBox1
+			// 
+			this.iTalk_RichTextBox1.AutoWordSelection = false;
+			this.iTalk_RichTextBox1.BackColor = System.Drawing.Color.Transparent;
+			this.iTalk_RichTextBox1.Font = new System.Drawing.Font("Tahoma", 10F);
+			this.iTalk_RichTextBox1.ForeColor = System.Drawing.Color.DimGray;
+			this.iTalk_RichTextBox1.Location = new System.Drawing.Point(483, 288);
+			this.iTalk_RichTextBox1.Name = "iTalk_RichTextBox1";
+			this.iTalk_RichTextBox1.ReadOnly = false;
+			this.iTalk_RichTextBox1.Size = new System.Drawing.Size(226, 67);
+			this.iTalk_RichTextBox1.TabIndex = 6;
+			this.iTalk_RichTextBox1.WordWrap = true;
+			// 
+			// flatLabel4
+			// 
+			this.flatLabel4.AutoSize = true;
+			this.flatLabel4.BackColor = System.Drawing.Color.Transparent;
+			this.flatLabel4.Font = new System.Drawing.Font("Segoe UI", 8F);
+			this.flatLabel4.ForeColor = System.Drawing.Color.White;
+			this.flatLabel4.Location = new System.Drawing.Point(398, 288);
+			this.flatLabel4.Name = "flatLabel4";
+			this.flatLabel4.Size = new System.Drawing.Size(57, 13);
+			this.flatLabel4.TabIndex = 7;
+			this.flatLabel4.Text = "Consejos:";
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Location = new System.Drawing.Point(57, 0);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(40, 27);
+			this.pictureBox2.TabIndex = 8;
+			this.pictureBox2.TabStop = false;
+			// 
+			// flatTabControl1
+			// 
+			this.flatTabControl1.ActiveColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(168)))), ((int)(((byte)(109)))));
+			this.flatTabControl1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(47)))), ((int)(((byte)(49)))));
+			this.flatTabControl1.Controls.Add(this.tabPage4);
+			this.flatTabControl1.Controls.Add(this.tabPage5);
+			this.flatTabControl1.Font = new System.Drawing.Font("Segoe UI", 10F);
+			this.flatTabControl1.ItemSize = new System.Drawing.Size(120, 40);
+			this.flatTabControl1.Location = new System.Drawing.Point(3, 0);
+			this.flatTabControl1.Name = "flatTabControl1";
+			this.flatTabControl1.SelectedIndex = 0;
+			this.flatTabControl1.Size = new System.Drawing.Size(735, 373);
+			this.flatTabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+			this.flatTabControl1.TabIndex = 0;
+			// 
+			// tabPage4
+			// 
+			this.tabPage4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(70)))), ((int)(((byte)(73)))));
+			this.tabPage4.Location = new System.Drawing.Point(4, 44);
+			this.tabPage4.Name = "tabPage4";
+			this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage4.Size = new System.Drawing.Size(727, 325);
+			this.tabPage4.TabIndex = 0;
+			this.tabPage4.Text = "VerGrupos";
+			// 
+			// tabPage5
+			// 
+			this.tabPage5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(70)))), ((int)(((byte)(73)))));
+			this.tabPage5.Location = new System.Drawing.Point(4, 44);
+			this.tabPage5.Name = "tabPage5";
+			this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage5.Size = new System.Drawing.Size(727, 325);
+			this.tabPage5.TabIndex = 1;
+			this.tabPage5.Text = "CrearGrupo";
+			// 
+			// ListaGrupos
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(746, 471);
+			this.Controls.Add(this.formSkin1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "ListaGrupos";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "ListaGrupos";
+			this.TransparencyKey = System.Drawing.Color.Fuchsia;
+			this.Alumnos.ResumeLayout(false);
+			this.tabPage2.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabPage1.PerformLayout();
+			this.formSkin1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			this.flatTabControl1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private FlatUI.FlatTabControl Alumnos;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private FlatUI.FlatLabel flatLabel4;
+		private iTalk.iTalk_RichTextBox iTalk_RichTextBox1;
+		private iTalk.iTalk_LinkLabel iTalk_LinkLabel1;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+		private FlatUI.FlatLabel flatLabel3;
+		private FlatUI.FlatLabel flatLabel2;
+		private FlatUI.FlatLabel flatLabel1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.TabPage tabPage3;
+		private FlatUI.FormSkin formSkin1;
+		private FlatUI.FlatClose flatClose1;
+		private FlatUI.FlatMax flatMax1;
+		private FlatUI.FlatMini flatMini1;
+		private FlatUI.FlatTabControl flatTabControl1;
+		private System.Windows.Forms.TabPage tabPage4;
+		private System.Windows.Forms.TabPage tabPage5;
 	}
 }
